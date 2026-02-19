02f7759dcc49896424fade3bd9c3f4cb2c61f365
Marc Anthony <mkamanzi@fltug.com>
2026-02-19 15:26:50 +0300

    favs section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
     <Hero/>
-    <div>
-        
+
+    <div id="favs-section" class="w-screen flex mx-auto pt-16">
+        <div class="w-full bg-rose-50 flex flex-col items-center ">
+            <h1 class="text-3xl font-black text-rose-500 ">
+                Find Your Favourites
+            </h1>
+
+            <div class="flex gap-4 justify-between">
+                <FavItems />
+            </div>
+        </div>
     </div>
 </template>
 
