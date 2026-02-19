e81ff67669c15000dd7d46957c9bef24a2b317ee
Marc Anthony <mkamanzi@fltug.com>
2026-02-19 17:29:47 +0300

    favs

--- components/FavItems.vue
+++ components/FavItems.vue
@@ -1,10 +1,33 @@
 <template>
-    <div>
-        <p>niufe</p>
+    <div class="flex flex-col gap-3 justify-center items-center">
+        <div 
+        class="w-28 h-28 rounded-full outline-rose-400 outline-dotted outline-offset-4  bg-cover bg-center transition-transform duration-300 hover:scale-105"
+        :style="bgStyle"></div>
+        <h3>{{name}}</h3>
     </div>
 </template>
 
-<script setup>
+<script>
+
+export default{
+    props: {
+    name: {
+      type: String,
+      default: 'Favourite Item'
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    bgStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      }
+    }
+  }
+}
 
 </script>
 
