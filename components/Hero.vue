31915ac3df2dbbe7d5fcb6dec568d950ce792910
Marc Anthony <mkamanzi@fltug.com>
2026-02-20 15:06:25 +0300

    added mobile responsiveness to current sections

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="w-screen bg-cover bg-bottom bg-[url(/assets/images/Union.svg)]">
-    <div class="flex h-[800px] w-full container mx-auto p-4 justify-between">
-        <div class="felx-col justify-center content-center px-auto">
-            <h1 class="heading text-8xl tracking-wide font-stretch-condensed">Chef Pearl's Bakes and more..</h1>
-            <p class="subheading uppercase font-bold text-3xl text-indigo-700 mt-4">Cakes, Pastries, Bisquits & Bread</p>
-            <div class="mt-9 flex gap-6">
+    <div class="flex flex-col md:flex-row h-[85vh] md:min-h-[80vh] w-full container mx-auto p-4 justify-between">
+      <!-- Text -->
+        <div class="flex-col justify-center content-center text-center md:text-left max-w-xl">
+            <h1 class="heading text-4xl sm:text-5xl md:text-7xl lg:text-8xl tracking-wide leading-tight">Chef Pearl's Bakes and more..</h1>
+            <p class="subheading uppercase font-bold text-xl sm:text-xl md:text-3xl text-indigo-700 mt-4">Cakes, Pastries, Bisquits & Bread</p>
+            <!-- buttons -->
+            <div class="mt-8 flex flex-col sm:flex-row gap-4 sm:gap-6 justify-center md:justify-start">
               <NuxtLink>
                 <BaseButton label="Explore my menus"/>
               </NuxtLink>
@@ -15,7 +17,8 @@
             </div>
         </div>
 
-        <div class=" w-[80%] h-[80%] self-center">
+        <!-- hero img -->
+        <div class="w-full md:w-[80%] h-full sm:h-[100%] md:h-[100%] self-center overflow-hidden">
             <div class="w-full h-full bg-cover bg-center masked-img bg-[url(/assets/images/chef.png)] transition-all duration-700 ease-out hover:scale-110 hover:bg-top"></div>
         </div>
     </div>
--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="text-white bg-rose-400 border-b border-dashed border-white h-24 place-content-center">
         <nav class="flex container mx-auto p-4 justify-between py-4 items-center">
+            <!-- logo -->
             <NuxtLink to="/" class="font-bold">
-                <div class="bg-center bg-center bg-no-repeat bg-bottom bg-[url(/assets/images/Logo.svg)] w-60 h-20"></div>
+                <div class="bg-center bg-center bg-no-repeat bg-bottom bg-[url(/assets/images/Logo.svg)] w-40 md:w-60 h-16 md:h-20"></div>
             </NuxtLink>
-            <ul class="flex gap-16">
+            <!-- Desktop nav -->
+            <ul class="hidden md:flex gap-16">
                 <li><NuxtLink to="/cakes">Cakes</NuxtLink></li>
                 <li><NuxtLink to="/pastries">Pastries</NuxtLink></li>
                 <li><NuxtLink to="/cookies">Cookies & Biscuits</NuxtLink></li>
                 <li><NuxtLink to="/breads">Breads</NuxtLink></li>
                 <li><NuxtLink to="/chefpearl">About Me</NuxtLink></li>
             </ul>
-            <NuxtLink>
-                <BaseButton label="Place your Order" variant="yellow"/>
-            </NuxtLink>
+            <!-- Desktop CTA -->
+             <div class="hidden md:block">
+                <NuxtLink>
+                    <BaseButton label="Place your Order" variant="yellow"/>
+                </NuxtLink>
+             </div>
+
+             <!-- Mobile Hamburger -->
+            <button class="md:hidden w-10 h-10 bg-no-repeat bg-center bg-contain hover:scale-105 transition-transform bg-[url(/assets/images/fastfood_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg)]"></button>
         </nav>
     </div>
 </template>
