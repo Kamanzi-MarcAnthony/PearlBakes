ba88755148dc25fb2cf1cee7dc0d245f9758d779
Marc Anthony <mkamanzi@fltug.com>
2026-02-23 14:52:36 +0300

    lunch section

--- components/LunchMenuItem.vue
+++ components/LunchMenuItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[300px] h-[300px] bg-white rounded-lg shadow-lg flex flex-col justify-center items-center gap-4">
+    <div class="w-[300px] h-[300px] bg-white rounded-lg shadow-lg flex flex-col justify-center items-center border border-black gap-4">
         <div class="w-24 h-24 rounded-full bg-cover bg-center bg-red-700"></div>
         <h3 class="text-xl font-bold">{{name}}</h3>
         
